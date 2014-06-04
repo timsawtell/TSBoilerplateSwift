6c522b6724c3be0e94b813cece62d107f5b8c617
Tim Sawtell <timsa@gji.com.au>
2014-06-04 14:28:45 +1000

    initial

---
+++ App/Controller/TopLevelControllers/TSViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TSViewController.swift
+//  TSBoilerplateSwift
+//
+//  Created by Tim Sawtell on 4/06/2014.
+//  Copyright (c) 2014 Sawtell Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TSViewController : UIViewController {
+    var scrollViewToResize: UIScrollView?
+}
