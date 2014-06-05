4f2ab84ec4157c30e5c576ab32119b944f110240
Tim Sawtell <timsa@gji.com.au>
2014-06-05 17:01:53 +1000

    started hacking up the mogen template

---
+++ App/Model/ModelObjects/Swift/_Book.h
@@ -0,0 +1,62 @@
+import Foundation
+
+class Book {
+
+	
+
+	
+		
+			let kModelPropertyBookAuthor= "author"
+		
+	
+		
+			let kModelPropertyBookBlurb= "blurb"
+		
+	
+		
+			let kModelPropertyBookPrice= "price"
+		
+	
+		
+			let kModelPropertyBookTitle= "title"
+		
+	
+
+	
+	
+	
+		
+			var author: NSString*
+		
+			
+		
+	
+		
+			var blurb: NSString*
+		
+			
+		
+	
+		
+			var price: NSString*
+		
+			
+		
+	
+		
+			var title: NSString*
+		
+			
+		
+	
+}
+
+
+
+
+
+
+
+
+
+@end
